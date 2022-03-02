537685fd97b73fbf5dfbaf979539ed66eae2ff06
Caufland <onimod05@gmail.com>
2022-03-03 00:13:20 +0100

    Sprzątanko

--- src/main/java/rpg/rpgcore/utils/Colorize.java
+++ src/main/java/rpg/rpgcore/utils/Colorize.java
@@ -4,10 +4,13 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class Colorize {
+
     public static String format(String str) {
         return ChatColor.translateAlternateColorCodes('&',str);
     }
+
     public static void sendMessage(CommandSender sender, String msg) {
         sender.sendMessage(format(msg));
     }
+
 }
