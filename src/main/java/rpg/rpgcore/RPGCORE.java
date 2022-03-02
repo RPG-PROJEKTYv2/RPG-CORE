e16c3dc5105a292fa05c1e96d77764fe0a621f48
Caufland <onimod05@gmail.com>
2022-03-02 21:42:30 +0100

    TEst

---
+++ src/main/java/rpg/rpgcore/RPGCORE.java
@@ -0,0 +1,15 @@
+package rpg.rpgcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPGCORE extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
