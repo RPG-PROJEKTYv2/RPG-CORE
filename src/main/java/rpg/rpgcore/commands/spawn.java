58c7b4ea0b8ad118cee48d39a1716a2a7d113c23
Caufland <onimod05@gmail.com>
2022-03-03 22:12:47 +0100

    + spawn

---
+++ src/main/java/rpg/rpgcore/commands/spawn.java
@@ -0,0 +1,13 @@
+package rpg.rpgcore.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class spawn implements CommandExecutor {
+
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        return false;
+    }
+}
