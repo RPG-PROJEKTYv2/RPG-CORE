7ff30e0c014be5be94d2fcdef24f6c01e9255e68
MiresPL <93918740+MiresPL@users.noreply.github.com>
2022-11-28 12:09:16 +0100

    rybak c.d

--- src/main/java/rpg/rpgcore/npc/rybak/PlayerFishListener.java
+++ src/main/java/rpg/rpgcore/npc/rybak/PlayerFishListener.java
@@ -1,7 +1,9 @@
 package rpg.rpgcore.npc.rybak;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.ArmorStand;
+import net.minecraft.server.v1_8_R3.EntityFishingHook;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
+import org.bukkit.entity.FishHook;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,9 +11,16 @@ import org.bukkit.event.player.PlayerFishEvent;
 import rpg.rpgcore.RPGCORE;
 import rpg.rpgcore.utils.Utils;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
 public class PlayerFishListener implements Listener {
 
     private final RPGCORE rpgcore;
+    private final Map<UUID, Long> timeMap = new HashMap<>();
 
     public PlayerFishListener(final RPGCORE rpgcore) {
         this.rpgcore = rpgcore;
@@ -19,24 +28,88 @@ public class PlayerFishListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onFish(final PlayerFishEvent e) {
+        final Player player = e.getPlayer();
         e.setExpToDrop(0);
-        int a = rpgcore.getServer().getScheduler().scheduleSyncRepeatingTask(rpgcore, () -> {
-            if (e.getState() == PlayerFishEvent.State.FAILED_ATTEMPT) {
-                rpgcore.getServer().getScheduler().cancelTask(rpgcore.getRybakNPC().getTaskId(e.getPlayer().getUniqueId()));
-                rpgcore.getRybakNPC().removeTaskId(e.getPlayer().getUniqueId());
-                return;
-            }
-            e.getPlayer().sendMessage(e.getHook().getBiteChance() + "");
-            e.getPlayer().sendMessage(e.getState().name());
-        }, 1L, 60L);
-        rpgcore.getRybakNPC().addTaskId(e.getPlayer().getUniqueId(), a);
         if (e.getState() == PlayerFishEvent.State.FISHING) {
-            //final ArmorStand as = (ArmorStand) Bukkit.getWorld(e.getPlayer().getWorld().getName()).spawnEntity(e.getHook().getLocation(), org.bukkit.entity.EntityType.ARMOR_STAND);
-            e.getHook().setCustomName(Utils.format("&4&lSIEMA"));
-           // rpgcore.getRybakNPC().addArmorStand(e.getPlayer().getUniqueId(), as);
+            this.runAnimation(e);
         }
         if (e.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
-            e.getHook().setCustomName(Utils.format("&a&lZlapano!"));
+            rpgcore.getServer().getScheduler().cancelTask(rpgcore.getRybakNPC().getTaskId(player.getUniqueId()));
+            rpgcore.getRybakNPC().removeTaskId(player.getUniqueId());
+            this.timeMap.remove(player.getUniqueId());
+            final int check = new Random().nextInt(4) + 1;
+            switch (check) {
+                case 1:
+                    rpgcore.getNmsManager().sendTitleAndSubTitle(player, rpgcore.getNmsManager().makeTitle("&6&lDOL", 5, 20, 5), rpgcore.getNmsManager().makeSubTitle("&7Spojrz w &6dol&7, zeby &apomyslnie &7wyciagnac rybe", 5, 20, 5));
+                    break;
+                case 2:
+                    rpgcore.getNmsManager().sendTitleAndSubTitle(player, rpgcore.getNmsManager().makeTitle("&6&lGORA", 5, 20, 5), rpgcore.getNmsManager().makeSubTitle("&7Spojrz w &6gore&7, zeby &apomyslnie &7wyciagnac rybe", 5, 20, 5));
+                    break;
+                case 3:
+                    rpgcore.getNmsManager().sendTitleAndSubTitle(player, rpgcore.getNmsManager().makeTitle("&6&lLEWO", 5, 20, 5), rpgcore.getNmsManager().makeSubTitle("&7Spojrz w &6lewo&7, zeby &apomyslnie &7wyciagnac rybe", 5, 20, 5));
+                    break;
+                case 4:
+                    rpgcore.getNmsManager().sendTitleAndSubTitle(player, rpgcore.getNmsManager().makeTitle("&6&lPRAWO", 5, 20, 5), rpgcore.getNmsManager().makeSubTitle("&7Spojrz w &6prawo&7, zeby &apomyslnie &7wyciagnac rybe", 5, 20, 5));
+                    break;
+            }
+            final float toCheckBefore = (check == 1 || check == 2 ? player.getLocation().getPitch() : player.getLocation().getYaw());
+            rpgcore.getServer().getScheduler().runTaskLater(rpgcore, () -> {
+                final float toCheckAfter = (check == 1 || check == 2 ? player.getLocation().getPitch() : player.getLocation().getYaw());
+                if (toCheckBefore == toCheckAfter) {
+                    player.sendMessage(Utils.format("&8[&c✘&8] &cNiestety ryba zerwala sie z linki..."));
+                }
+
+                if (check == 1) {
+                    //TODO dokonczyc sprawdzanie gora.dol.lewo.prawo
+                }
+            }, 40L);
         }
+    }
+
+    private void runAnimation(final PlayerFishEvent e) {
+        this.timeMap.put(e.getPlayer().getUniqueId(), System.currentTimeMillis() + 15000);
+        this.setBiteTime(e.getHook(), 360);
+        int a = rpgcore.getServer().getScheduler().scheduleSyncRepeatingTask(rpgcore, () -> {
+            final String time = Utils.durationToString(this.timeMap.get(e.getPlayer().getUniqueId()), false);
+            e.getPlayer().sendMessage(e.getState().name()); //TODO <- PRZETESTOWAC!
+            if (e.getState() == PlayerFishEvent.State.FAILED_ATTEMPT || e.getState() == PlayerFishEvent.State.CAUGHT_FISH || e.getState() == PlayerFishEvent.State.IN_GROUND) {
+                rpgcore.getServer().getScheduler().cancelTask(rpgcore.getRybakNPC().getTaskId(e.getPlayer().getUniqueId()));
+                rpgcore.getRybakNPC().removeTaskId(e.getPlayer().getUniqueId());
+                this.timeMap.remove(e.getPlayer().getUniqueId());
+                return;
+            }
+            if (time.equals("<1s")) {
+                e.getHook().setCustomName(Utils.format("&c" + e.getPlayer().getName() + " &eplynie..."));
+                return;
+            }
+            e.getHook().setCustomName(Utils.format("&c" + e.getPlayer().getName() + " &7oczekuje na rybe... &c" + time));
+        }, 1L, 20L);
+        if (rpgcore.getRybakNPC().getTaskMap().containsKey(e.getPlayer().getUniqueId())) {
+            rpgcore.getServer().getScheduler().cancelTask(rpgcore.getRybakNPC().getTaskId(e.getPlayer().getUniqueId()));
+            rpgcore.getRybakNPC().removeTaskId(e.getPlayer().getUniqueId());
+        }
+        rpgcore.getRybakNPC().addTaskId(e.getPlayer().getUniqueId(), a);
+    }
+
+    private void setBiteTime(FishHook hook, int time) {
+        net.minecraft.server.v1_8_R3.EntityFishingHook hookCopy = (EntityFishingHook) ((CraftEntity) hook).getHandle();
+
+        Field fishCatchTime = null;
+
+        try {
+            fishCatchTime = net.minecraft.server.v1_8_R3.EntityFishingHook.class.getDeclaredField("aw");
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        }
+
+        fishCatchTime.setAccessible(true);
+
+        try {
+            fishCatchTime.setInt(hookCopy, time);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        fishCatchTime.setAccessible(false);
     }
 }
