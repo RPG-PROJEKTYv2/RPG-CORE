2ef0349b50565761b2430431c2d71a44c5074a41
MiresPL <mateusz.skorek@tm1.edu.pl>
2023-04-23 21:54:47 +0200

    przerobione na itemy w listanpc

--- src/main/java/rpg/rpgcore/listanpc/ListaNPCManager.java
+++ src/main/java/rpg/rpgcore/listanpc/ListaNPCManager.java
@@ -3,6 +3,7 @@ package rpg.rpgcore.listanpc;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -12,11 +13,13 @@ import rpg.rpgcore.utils.Utils;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class ListaNPCManager {
     private final RPGCORE rpgcore;
     private final Map<Integer, NpcObject> npcMap;
+
     public ListaNPCManager(final RPGCORE rpgcore) {
         this.rpgcore = rpgcore;
         this.npcMap = this.loadAll();
@@ -26,15 +29,18 @@ public class ListaNPCManager {
         final Map<Integer, NpcObject> toLoad = new HashMap<>();
         int i = 1;
         for (String s : rpgcore.getConfig().getConfigurationSection("ListaNPC").getKeys(false)) {
+            final ConfigurationSection section = rpgcore.getConfig().getConfigurationSection("ListaNPC").getConfigurationSection(s);
+            final Material material = Material.getMaterial(section.getConfigurationSection("Item").getString("Material").toUpperCase(Locale.ROOT));
+            System.out.println(material);
+            final ItemStack guiItem = (section.getConfigurationSection("Item").getInt("Short") == 0 ? new ItemStack(material, 1) : new ItemStack(material, 1, (short) section.getConfigurationSection("Item").getInt("Short")));
+
             toLoad.put(i, new NpcObject(s, new Location(
-                    Bukkit.getWorld(rpgcore.getConfig().getConfigurationSection("ListaNPC").getConfigurationSection(s).getString("world")),
-                    rpgcore.getConfig().getConfigurationSection("ListaNPC").getConfigurationSection(s).getDouble("x"),
-                    rpgcore.getConfig().getConfigurationSection("ListaNPC").getConfigurationSection(s).getDouble("y"),
-                    rpgcore.getConfig().getConfigurationSection("ListaNPC").getConfigurationSection(s).getDouble("z"),
-                    rpgcore.getConfig().getConfigurationSection("ListaNPC").getConfigurationSection(s).getLong("yaw"),
-                    rpgcore.getConfig().getConfigurationSection("ListaNPC").getConfigurationSection(s).getLong("pitch")
-                    ), rpgcore.getConfig().getConfigurationSection("ListaNPC").getConfigurationSection(s).getString("Type"),
-                    rpgcore.getConfig().getConfigurationSection("ListaNPC").getConfigurationSection(s).getInt("ReqLvl")));
+                    Bukkit.getWorld(section.getString("world")),
+                    section.getDouble("x"),
+                    section.getDouble("y"),
+                    section.getDouble("z"),
+                    section.getLong("yaw"),
+                    section.getLong("pitch")), section.getString("Type"), section.getInt("ReqLvl"), guiItem));
             i++;
         }
 
@@ -46,25 +52,31 @@ public class ListaNPCManager {
         final Inventory gui = Bukkit.createInventory(null, 27, Utils.format("&cLista NPC"));
         final int lvl = rpgcore.getUserManager().find(player.getUniqueId()).getLvl();
         for (int i = 0; i < npcMap.size(); i++) {
-            gui.setItem(i, this.makeNpcItem(this.npcMap.get(i+1), lvl));
+            gui.setItem(i, this.makeNpcItem(this.npcMap.get(i + 1), lvl));
         }
 
         player.openInventory(gui);
     }
 
     private ItemStack makeNpcItem(final NpcObject npc, final int lvl) {
-        short durability = 14;
-        if (lvl >= npc.getReqLvl()) {
-            durability = 5;
-        }
-        return new ItemBuilder(Material.STAINED_GLASS_PANE, 1, durability).setName("&6" + npc.getNpcName()).setLore(Arrays.asList(
-                "&7Lokalizacja:",
-                "&7x: &6" + npc.getLocation().getX(),
-                "&7y: &6" + npc.getLocation().getY(),
-                "&7z: &6" + npc.getLocation().getZ(),
-                "",
-                "&7Typ Npc: &c" + npc.getNpcType()
-                )).addGlowing().toItemStack().clone();
+        return (lvl >= npc.getReqLvl() ?
+                new ItemBuilder(npc.getGuiItem()).setName("&6" + npc.getNpcName()).setLore(Arrays.asList(
+                        "&7Lokalizacja:",
+                        "&7x: &6" + npc.getLocation().getX(),
+                        "&7y: &6" + npc.getLocation().getY(),
+                        "&7z: &6" + npc.getLocation().getZ(),
+                        "",
+                        "&7Typ Npc: &c" + npc.getNpcType()
+                )).addGlowing().toItemStack().clone()
+                :
+                new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (short) 14).setName("&6" + npc.getNpcName()).setLore(Arrays.asList(
+                        "&7Lokalizacja:",
+                        "&7x: &6" + npc.getLocation().getX(),
+                        "&7y: &6" + npc.getLocation().getY(),
+                        "&7z: &6" + npc.getLocation().getZ(),
+                        "",
+                        "&7Typ Npc: &c" + npc.getNpcType()
+                )).addGlowing().toItemStack().clone());
     }
 
     public NpcObject find(final int i) {
--- src/main/java/rpg/rpgcore/listanpc/NpcObject.java
+++ src/main/java/rpg/rpgcore/listanpc/NpcObject.java
@@ -3,6 +3,7 @@ package rpg.rpgcore.listanpc;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
 
 @Getter
 @Setter
@@ -11,11 +12,13 @@ public class NpcObject {
     private final Location location;
     private final String npcType;
     private final int reqLvl;
+    private final ItemStack guiItem;
 
-    public NpcObject(final String npcName, final Location location, final String npcType, final int reqLvl) {
+    public NpcObject(final String npcName, final Location location, final String npcType, final int reqLvl, final ItemStack guiItem) {
         this.npcName = npcName;
         this.location = location;
         this.npcType = npcType;
         this.reqLvl = reqLvl;
+        this.guiItem = guiItem;
     }
 }
