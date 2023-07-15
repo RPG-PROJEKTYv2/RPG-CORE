0f57b613c127c162c1c47242a7649d08cf34c01b
ZwariowanyOrzel <103900500+ZwariowanyOrzel@users.noreply.github.com>
2023-07-15 16:27:57 +0200

    handlarz itemy

--- src/main/java/rpg/rpgcore/npc/handlarz/enums/HandlarzSellItems.java
+++ src/main/java/rpg/rpgcore/npc/handlarz/enums/HandlarzSellItems.java
@@ -19,15 +19,15 @@ public enum HandlarzSellItems {
     I9("Buty Dowodcy Rozbojnikow", Material.LEATHER_BOOTS, 12.0),
     I10("Miecz Dowodcy Rozbojnikow", Material.WOOD_SWORD, 15.0),
     // 1-10 BOSS AKCE ZWYKŁE
-    I11("Zwykly Naszyjnik Dowodcy Rozbojnikow", Material.STORAGE_MINECART, 25.0),
-    I12("Zwykly Diadem Dowodcy Rozbojnikow", Material.WATCH, 20.0),
-    I13("Zwykly Pierscien Dowodcy Rozbojnikow", Material.EXPLOSIVE_MINECART, 20.0),
-    I14("Zwykla Tarcza Dowodcy Rozbojnikow", Material.ITEM_FRAME, 25.0),
+    I11("Zwykly Naszyjnik Dowodcy Rozbojnikow", Material.STORAGE_MINECART, 100.0),
+    I12("Zwykly Diadem Dowodcy Rozbojnikow", Material.WATCH, 100.0),
+    I13("Zwykly Pierscien Dowodcy Rozbojnikow", Material.EXPLOSIVE_MINECART, 100.0),
+    I14("Zwykla Tarcza Dowodcy Rozbojnikow", Material.ITEM_FRAME, 100.0),
     // 1-10 BOSS AKCE ULEPSZONE
-    I15("Ulepszony Naszyjnik Dowodcy Rozbojnikow", Material.STORAGE_MINECART, 35.0),
-    I16("Ulepszony Diadem Dowodcy Rozbojnikow", Material.WATCH, 30.0),
-    I17("Ulepszony Pierscien Dowodcy Rozbojnikow", Material.EXPLOSIVE_MINECART, 30.0),
-    I18("Ulepszona Tarcza Dowodcy Rozbojnikow", Material.ITEM_FRAME, 35.0),
+    I15("Ulepszony Naszyjnik Dowodcy Rozbojnikow", Material.STORAGE_MINECART, 120.0),
+    I16("Ulepszony Diadem Dowodcy Rozbojnikow", Material.WATCH, 120.0),
+    I17("Ulepszony Pierscien Dowodcy Rozbojnikow", Material.EXPLOSIVE_MINECART, 120.0),
+    I18("Ulepszona Tarcza Dowodcy Rozbojnikow", Material.ITEM_FRAME, 120.0),
     
     // 10-20 MOB
     I19("Helm Goblina", Material.LEATHER_HELMET, 15.0),
@@ -42,15 +42,15 @@ public enum HandlarzSellItems {
     I27("Buty Wodza Goblinow", Material.LEATHER_BOOTS, 19.0),
     I28("Miecz Wodza Goblinow", Material.WOOD_SWORD, 23.0),
     // 10-20 BOSS AKCE ZWYKŁE
-    I29("Zwykly Naszyjnik Wodza Goblinow", Material.STORAGE_MINECART, 35.0),
-    I30("Zwykly Diadem Wodza Goblinow", Material.WATCH, 30.0),
-    I31("Zwykly Pierscien Wodza Goblinow", Material.EXPLOSIVE_MINECART, 30.0),
-    I32("Zwykla Tarcza Wodza Goblinow", Material.ITEM_FRAME, 35.0),
+    I29("Zwykly Naszyjnik Wodza Goblinow", Material.STORAGE_MINECART, 120.0),
+    I30("Zwykly Diadem Wodza Goblinow", Material.WATCH, 120.0),
+    I31("Zwykly Pierscien Wodza Goblinow", Material.EXPLOSIVE_MINECART, 120.0),
+    I32("Zwykla Tarcza Wodza Goblinow", Material.ITEM_FRAME, 120.0),
     // 10-20 BOSS AKCE ULEPSZONE
-    I33("Ulepszony Naszyjnik Wodza Goblinow", Material.STORAGE_MINECART, 50.0),
-    I34("Ulepszony Diadem Wodza Goblinow", Material.WATCH, 45.0),
-    I35("Ulepszony Pierscien Wodza Goblinow", Material.EXPLOSIVE_MINECART, 45.0),
-    I36("Ulepszona Tarcza Wodza Goblinow", Material.ITEM_FRAME, 50.0),
+    I33("Ulepszony Naszyjnik Wodza Goblinow", Material.STORAGE_MINECART, 180.0),
+    I34("Ulepszony Diadem Wodza Goblinow", Material.WATCH, 180.0),
+    I35("Ulepszony Pierscien Wodza Goblinow", Material.EXPLOSIVE_MINECART, 180.0),
+    I36("Ulepszona Tarcza Wodza Goblinow", Material.ITEM_FRAME, 180.0),
 
     // 20-30 MOB
     I37("Helm Goryla", Material.LEATHER_HELMET, 17.0),
@@ -65,17 +65,115 @@ public enum HandlarzSellItems {
     I45("Buty Krola Goryli", Material.GOLD_BOOTS, 21.0),
     I46("Miecz Krola Goryli", Material.GOLD_SWORD, 25.0),
     // 20-30 BOSS AKCE ZWYKŁE
-    I47("Zwykly Naszyjnik Krola Goryli", Material.STORAGE_MINECART, 45.0),
-    I48("Zwykly Diadem Krola Goryli", Material.WATCH, 40.0),
-    I49("Zwykly Pierscien Krola Goryli", Material.EXPLOSIVE_MINECART, 40.0),
-    I50("Zwykla Tarcza Krola Goryli", Material.ITEM_FRAME, 45.0),
+    I47("Zwykly Naszyjnik Krola Goryli", Material.STORAGE_MINECART, 200.0),
+    I48("Zwykly Diadem Krola Goryli", Material.WATCH, 200.0),
+    I49("Zwykly Pierscien Krola Goryli", Material.EXPLOSIVE_MINECART, 200.0),
+    I50("Zwykla Tarcza Krola Goryli", Material.ITEM_FRAME, 200.0),
     // 20-30 BOSS AKCE ULEPSZONE
-    I51("Ulepszony Naszyjnik Krola Goryli", Material.STORAGE_MINECART, 65.0),
-    I52("Ulepszony Diadem Krola Goryli", Material.WATCH, 60.0),
-    I53("Ulepszony Pierscien Krola Goryli", Material.EXPLOSIVE_MINECART, 60.0),
-    I54("Ulepszona Tarcza Krola Goryli", Material.ITEM_FRAME, 65.0),
+    I51("Ulepszony Naszyjnik Krola Goryli", Material.STORAGE_MINECART, 250.0),
+    I52("Ulepszony Diadem Krola Goryli", Material.WATCH, 250.0),
+    I53("Ulepszony Pierscien Krola Goryli", Material.EXPLOSIVE_MINECART, 250.0),
+    I54("Ulepszona Tarcza Krola Goryli", Material.ITEM_FRAME, 250.0),
 
     // 30-40 MOB
+    I55("Helm Zjawy", Material.LEATHER_HELMET, 30.0),
+    I56("Zbroja Zjawy", Material.CHAINMAIL_CHESTPLATE,39.0),
+    I57("Spodnie Zjawy", Material.LEATHER_LEGGINGS,  38.0),
+    I58("Buty Zjawy", Material.LEATHER_BOOTS, 36.0),
+    I59("Miecz Zjawy", Material.IRON_SWORD, 30.0),
+    // 30-40 BOSS
+    I60("Helm Przekletej Duszy", Material.GOLD_HELMET, 52.0),
+    I61("Zbroja Przekletej Duszy", Material.GOLD_CHESTPLATE, 74.0),
+    I62("Spodnie Przekletej Duszy", Material.GOLD_LEGGINGS, 53.0),
+    I63("Buty Przekletej Duszy", Material.GOLD_BOOTS, 41.0),
+    I64("Miecz Przekletej Duszy", Material.GOLD_SWORD, 65.0),
+    // 30-40 BOSS AKCE ZWYKLE
+    I65("Zwykly Naszyjnik Przekletej Duszy", Material.STORAGE_MINECART, 300.0),
+    I66("Zwykly Diadem Przekletej Duszy", Material.WATCH, 300.0),
+    I67("Zwykly Pierscien Przekletej Duszy", Material.EXPLOSIVE_MINECART, 300.0),
+    I68("Zwykla Tarcza Przekletej Duszy", Material.ITEM_FRAME, 300.0),
+    // 30-40 BOSS AKCE ULEPSZONE
+    I69("Ulepszony Naszyjnik Przekletej Duszy", Material.STORAGE_MINECART, 400.0),
+    I70("Ulepszony Diadem Przekletej Duszy", Material.WATCH, 400.0),
+    I71("Ulepszony Pierscien Przekletej Duszy", Material.EXPLOSIVE_MINECART, 400.0),
+    I72("Ulepszona Tarcza Przekletej Duszy", Material.ITEM_FRAME, 400.0),
+
+    // 40-50 MOB
+    I73("Helm Straznika Swiatyni", Material.LEATHER_HELMET, 67.0),
+    I74("Zbroja Straznika Swiatyni", Material.CHAINMAIL_CHESTPLATE,79.0),
+    I75("Spodnie Straznika Swiatyni", Material.LEATHER_LEGGINGS,  58.0),
+    I76("Buty Straznika Swiatyni", Material.LEATHER_BOOTS, 66.0),
+    I77("Miecz Straznika Swiatyni", Material.IRON_SWORD, 70.0),
+    // 40-50 BOSS
+    I78("Helm Trytona", Material.GOLD_HELMET, 62.0),
+    I79("Zbroja Trytona", Material.GOLD_CHESTPLATE, 84.0),
+    I80("Spodnie Trytona", Material.GOLD_LEGGINGS, 73.0),
+    I81("Buty Trytona", Material.GOLD_BOOTS, 84.0),
+    I82("Miecz Trytona", Material.GOLD_SWORD, 95.0),
+    // 40-50 BOSS AKCE ZWYKLE
+    I83("Zwykly Naszyjnik Trytona", Material.STORAGE_MINECART, 500.0),
+    I84("Zwykly Diadem Trytona", Material.WATCH, 500.0),
+    I85("Zwykly Pierscien Trytona", Material.EXPLOSIVE_MINECART, 500.0),
+    I86("Zwykla Tarcza Trytona", Material.ITEM_FRAME, 500.0),
+    // 40-50 BOSS AKCE ULEPSZONE
+    I87("Ulepszony Naszyjnik Trytona", Material.STORAGE_MINECART, 700.0),
+    I88("Ulepszony Diadem Trytona", Material.WATCH, 700.0),
+    I89("Ulepszony Pierscien Trytona", Material.EXPLOSIVE_MINECART, 700.0),
+    I90("Ulepszona Tarcza Trytona", Material.ITEM_FRAME, 700.0),
+
+    // 50-60 MOB
+    I91("Helm Lodowego Slugi", Material.LEATHER_HELMET, 107.0),
+    I92("Zbroja Lodowego Slugi", Material.CHAINMAIL_CHESTPLATE,128.0),
+    I93("Spodnie Lodowego Slugi", Material.LEATHER_LEGGINGS,  118.0),
+    I94("Buty Lodowego Slugi", Material.LEATHER_BOOTS, 106.0),
+    I95("Miecz Lodowego Slugi", Material.IRON_SWORD, 130.0),
+    // 50-60 MOB ICETOWER
+    I96("Helm Mroznego Wilka", Material.LEATHER_HELMET, 137.0),
+    I97("Zbroja Mroznego Wilka", Material.CHAINMAIL_CHESTPLATE,149.0),
+    I98("Spodnie Mroznego Wilka", Material.LEATHER_LEGGINGS,  138.0),
+    I99("Buty Mroznego Wilka", Material.LEATHER_BOOTS, 126.0),
+    I100("Wilczy Kiel", Material.IRON_SWORD, 170.0),
+    // 50-60 BOSS ICETOWER
+    I101("Helm Krola Lodu", Material.GOLD_HELMET, 162.0),
+    I102("Zbroja Krola Lodu", Material.GOLD_CHESTPLATE, 184.0),
+    I103("Spodnie Krola Lodu", Material.GOLD_LEGGINGS, 173.0),
+    I104("Buty Krola Lodu", Material.GOLD_BOOTS, 184.0),
+    I105("Miecz Krola Lodu", Material.GOLD_SWORD, 195.0),
+    // 50-60 BOSS AKCE ZWYKLE ICETOWER
+    I106("Zwykly Naszyjnik Krola Lodu", Material.STORAGE_MINECART, 800.0),
+    I107("Zwykly Diadem Krola Lodu", Material.WATCH, 800.0),
+    I108("Zwykly Pierscien Krola Lodu", Material.EXPLOSIVE_MINECART, 800.0),
+    I109("Zwykla Tarcza Krola Lodu", Material.ITEM_FRAME, 800.0),
+    // 50-60 BOSS AKCE ULEPSZONE ICETOWER
+    I110("Ulepszony Naszyjnik Krola Lodu", Material.STORAGE_MINECART, 1000.0),
+    I111("Ulepszony Diadem Krola Lodu", Material.WATCH, 1000.0),
+    I112("Ulepszony Pierscien Krola Lodu", Material.EXPLOSIVE_MINECART, 1000.0),
+    I113("Ulepszona Tarcza Krola Lodu", Material.ITEM_FRAME, 1000.0),
+
+
+    // 60-70 MOB
+    I114("Helm Zywiolaka Ognia", Material.LEATHER_HELMET, 267.0),
+    I115("Zbroja Zywiolaka Ognia", Material.CHAINMAIL_CHESTPLATE,279.0),
+    I116("Spodnie Zywiolaka Ognia", Material.LEATHER_LEGGINGS,  258.0),
+    I117("Buty Zywiolaka Ognia", Material.LEATHER_BOOTS, 266.0),
+    I118("Miecz Zywiolaka Ognia", Material.IRON_SWORD, 270.0),
+    // 60-70 BOSS
+    I119("Helm Piekielnego Rycerza", Material.GOLD_HELMET, 282.0),
+    I120("Zbroja Piekielnego Rycerza", Material.GOLD_CHESTPLATE, 284.0),
+    I121("Spodnie Piekielnego Rycerza", Material.GOLD_LEGGINGS, 273.0),
+    I122("Buty Piekielnego Rycerza", Material.GOLD_BOOTS, 284.0),
+    I123("Miecz Piekielnego Rycerza", Material.GOLD_SWORD, 295.0),
+    // 60-70 BOSS AKCE ZWYKLE
+    I124("Zwykly Naszyjnik Piekielnego Rycerza", Material.STORAGE_MINECART, 1200.0),
+    I125("Zwykly Diadem Piekielnego Rycerza", Material.WATCH, 1200.0),
+    I126("Zwykly Pierscien Piekielnego Rycerza", Material.EXPLOSIVE_MINECART, 1200.0),
+    I127("Zwykla Tarcza Piekielnego Rycerza", Material.ITEM_FRAME, 1200.0),
+    // 60-70 BOSS AKCE ULEPSZONE
+    I128("Ulepszony Naszyjnik Piekielnego Rycerza", Material.STORAGE_MINECART, 1600.0),
+    I129("Ulepszony Diadem Piekielnego Rycerza", Material.WATCH, 1600.0),
+    I130("Ulepszony Pierscien Piekielnego Rycerza", Material.EXPLOSIVE_MINECART, 1600.0),
+    I131("Ulepszona Tarcza Piekielnego Rycerza", Material.ITEM_FRAME, 1600.0),
+    
     I_ERROR("ERROR", Material.AIR, 0);
     private final String name;
     private final Material material;
