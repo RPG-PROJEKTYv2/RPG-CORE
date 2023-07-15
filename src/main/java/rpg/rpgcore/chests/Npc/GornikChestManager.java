37222bae05f89c54db24a2968c8f26a2a58f12e0
ZwariowanyOrzel <103900500+ZwariowanyOrzel@users.noreply.github.com>
2023-07-15 16:38:38 +0200

    gornik % lepsze

--- src/main/java/rpg/rpgcore/chests/Npc/GornikChestManager.java
+++ src/main/java/rpg/rpgcore/chests/Npc/GornikChestManager.java
@@ -13,22 +13,22 @@ public class GornikChestManager {
     private final Set<Items> gorniknpc = Sets.newConcurrentHashSet();
 
     public GornikChestManager() {
-        this.gorniknpc.add(new Items("1", 8.0, GornikItems.getItem("Ruda_Wegla", 1), 1));
-        this.gorniknpc.add(new Items("2", 7.0, GornikItems.getItem("Ruda_Zelaza", 1), 1));
-        this.gorniknpc.add(new Items("3", 6.0, GornikItems.getItem("Ruda_Topazu", 1), 1));
-        this.gorniknpc.add(new Items("4", 5.0, GornikItems.getItem("Ruda_Tanzanitu", 1), 1));
-        this.gorniknpc.add(new Items("5", 4.0, GornikItems.getItem("Ruda_Jadeitu", 1), 1));
-        this.gorniknpc.add(new Items("6", 4.0, GornikItems.getItem("Ruda_Wegla", 1), 2));
-        this.gorniknpc.add(new Items("7", 3.5, GornikItems.getItem("Ruda_Zelaza", 1), 2));
-        this.gorniknpc.add(new Items("8", 3.0, GornikItems.getItem("Ruda_Topazu", 1), 2));
-        this.gorniknpc.add(new Items("9", 3.0, GornikItems.getItem("Ruda_Diamentu", 1), 1));
-        this.gorniknpc.add(new Items("10", 2.5, GornikItems.getItem("Ruda_Tanzanitu", 1), 2));
-        this.gorniknpc.add(new Items("11", 2.0, GornikItems.getItem("Ruda_Runinu", 1), 1));
-        this.gorniknpc.add(new Items("12", 2.0, GornikItems.getItem("Ruda_Jadeitu", 1), 2));
-        this.gorniknpc.add(new Items("13", 1.5, GornikItems.getItem("Ruda_Diamentu", 1), 2));
-        this.gorniknpc.add(new Items("14", 1.0, GornikItems.getItem("Ruda_Runinu", 1), 2));
-        this.gorniknpc.add(new Items("15", 4.0, GornikItems.getItem("Skrzynia_Gornika", 1), 2));
-        this.gorniknpc.add(new Items("16", 3.0, GornikItems.getItem("Skrzynia_Gornika", 1), 3));
+        this.gorniknpc.add(new Items("1", 2.0, GornikItems.getItem("Ruda_Runinu", 1), 5));
+        this.gorniknpc.add(new Items("2", 2.5, GornikItems.getItem("Ruda_Diamentu", 1), 5));
+        this.gorniknpc.add(new Items("3", 3.0, GornikItems.getItem("Ruda_Runinu", 1), 3));
+        this.gorniknpc.add(new Items("4", 3.0, GornikItems.getItem("Ruda_Jadeitu", 1), 5));
+        this.gorniknpc.add(new Items("5", 3.5, GornikItems.getItem("Ruda_Tanzanitu", 1), 5));
+        this.gorniknpc.add(new Items("6", 4.0, GornikItems.getItem("Skrzynia_Gornika", 1), 3));
+        this.gorniknpc.add(new Items("7", 4.0, GornikItems.getItem("Ruda_Topazu", 1), 5));
+        this.gorniknpc.add(new Items("8", 4.0, GornikItems.getItem("Ruda_Diamentu", 1), 3));
+        this.gorniknpc.add(new Items("9", 4.5, GornikItems.getItem("Ruda_Zelaza", 1), 5));
+        this.gorniknpc.add(new Items("10", 5.0, GornikItems.getItem("Ruda_Jadeitu", 1), 3));
+        this.gorniknpc.add(new Items("11", 5.0, GornikItems.getItem("Ruda_Wegla", 1), 5));
+        this.gorniknpc.add(new Items("12", 5.0, GornikItems.getItem("Skrzynia_Gornika", 1), 2));
+        this.gorniknpc.add(new Items("13", 6.0, GornikItems.getItem("Ruda_Tanzanitu", 1), 3));
+        this.gorniknpc.add(new Items("14", 7.0, GornikItems.getItem("Ruda_Topazu", 1), 3));
+        this.gorniknpc.add(new Items("15", 8.0, GornikItems.getItem("Ruda_Zelaza", 1), 3));
+        this.gorniknpc.add(new Items("16", 9.0, GornikItems.getItem("Ruda_Wegla", 1), 3));
     }
 
 
